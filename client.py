d9ecc156178bfa01de0620a468e2024c3f9fc5db
todd <todd.develop@gmail.com>
2022-05-10 21:09:07 +0300

    qwe

--- client.py
+++ client.py
@@ -9,15 +9,10 @@ import eapteka_pb2_grpc
 
 def get_data():
     name = "abakan"
-    if os.environ.get('https_proxy'):
-        del os.environ['https_proxy']
-    if os.environ.get('http_proxy'):
-        del os.environ['http_proxy']
 
     with grpc.insecure_channel("141.101.188.7:8088", options=(("grpc.enable_http_proxy", 0),)) as ch:
         stub = eapteka_pb2_grpc.RunParserStub(ch)
         response = stub.run_parser(eapteka_pb2.City(name=name))
-
         print(response)
     # except KeyboardInterrupt:
         # ch.unsubscribe(close)
