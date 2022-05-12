d851e1207dc02bc1f559b1ed6bcba054e4a4ea88
todd <todd.develop@gmail.com>
2022-05-12 16:45:12 +0300

    upd

--- parser/conf_selenium.py
+++ parser/conf_selenium.py
@@ -1,7 +1,6 @@
 import os
 from base64 import b64encode
 
-from seleniumwire import webdriver as webd
 from selenium import webdriver
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -9,11 +8,22 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def get_web_driver(proxy=None):
     root_path = os.getcwd()
-    options = webd.FirefoxOptions()
+    h = "http://QPYXyF:rGurC6@193.36.58.208:8000"
+    proxy = Proxy(
+        {
+            'proxyType': ProxyType.MANUAL,
+            'httpProxy': h,
+            'ftpProxy': h,
+            'sslProxy': h,
+            'noProxy': ''
+        }
+    )
+
+    options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
     options.set_preference("dom.webdriver.enabled", False)
 
-    profile = webd.FirefoxProfile()
+    profile = webdriver.FirefoxProfile()
     profile.set_preference('dom.webdriver.enabled', False)
     profile.set_preference('useAutomationExtension', False)
     profile.set_preference("browser.cache.disk.enable", False)
@@ -21,13 +31,12 @@ def get_web_driver(proxy=None):
     profile.set_preference("browser.cache.offline.enable", False)
     profile.set_preference("network.http.use-cache", False)
 
-    driver = webd.Firefox(
+    driver = webdriver.Firefox(
         executable_path=root_path + '/geckodriver',
         options=options,
         firefox_profile=profile,
-        seleniumwire_options=proxy,
         desired_capabilities=DesiredCapabilities.FIREFOX,
-        # proxy=proxy_config,
+        proxy=proxy,
     )
     driver.set_page_load_timeout(3600 * 2 * 2)
     return driver
