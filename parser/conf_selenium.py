80a116d0cf57039e49b05de38268bebdfe66aa6c
todd <todd.develop@gmail.com>
2022-05-12 16:57:06 +0300

    upd

--- parser/conf_selenium.py
+++ parser/conf_selenium.py
@@ -6,20 +6,22 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-def get_web_driver(proxy=None):
+def get_web_driver(proxy_=None):
     root_path = os.getcwd()
-    h = "193.36.58.208:8000"
+    host = "193.36.58.208:8000"
     login = "QPYXyF"
-    p = "rGurC6"
-    proxy = Proxy()
-    proxy.proxy_type = ProxyType.MANUAL
-    proxy.http_proxy = h
-    proxy.socks_proxy = h
-    proxy.ssl_proxy = h
-    proxy.socksUsername = login
-    proxy.socksPassword = p
+    password = "rGurC6"
+    # proxy = Proxy()
     capabilities = DesiredCapabilities.FIREFOX
-    proxy.add_to_capabilities(capabilities)
+    capabilities['proxy'] = {
+        "proxyType": "MANUAL",
+        "httpProxy": host,
+        "ftpProxy": host,
+        "sslProxy": host,
+        "socksUsername": login,
+        "socksPassword": password,
+    }
+    # proxy.add_to_capabilities(capabilities)
 
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
@@ -38,7 +40,7 @@ def get_web_driver(proxy=None):
         options=options,
         firefox_profile=profile,
         desired_capabilities=capabilities,
-        proxy=proxy,
+        # proxy=proxy,
     )
     driver.set_page_load_timeout(3600 * 2 * 2)
     return driver
